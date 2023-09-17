50d65593358b998cec0f5fa6825d7a46aa154d18
Trần Học <hoc.st.it@gmail.com>
2023-09-17 17:27:40 +0700

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-tour-of-heros';
+  title = 'ljkh gfdk;hu gsd  hil;sgd hli;fd';
 }
+// @NgModule({
+//   imports: [
+//     HttpClientModule,
+//   ],
+// })
